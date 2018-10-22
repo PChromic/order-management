e8c4c84a73d67ce3042f5f7819ef361f19fa407f
PChromic <pchromicz@gmail.com>
2018-10-22 18:41:17 +0200

    Fixed .gitingore

--- src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
-insert into order_table (client_id,request_id,name,quantity,price) values ('1',1,'Bulka',1,10.11);
-insert into order_table (client_id,request_id,name,quantity,price) values ('1',1,'Chleb',2,15.11);
-insert into order_table (client_id,request_id,name,quantity,price) values ('1',2,'Chleb',5,15.00);
-insert into order_table (client_id,request_id,name,quantity,price) values ('2',1,'Chleb',1,10.00);
--- src/test/java/pchromic/validator/ValidatorTest.java
+++
@@ -1,5 +0,0 @@
-package pchromic.validator;
-
-class ValidatorTest {
-
-}
